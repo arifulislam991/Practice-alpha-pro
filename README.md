0c9e1e3f0d2b62a7aad4623bede20c7c3e8bd461
arifulislam991 <arifulislam99101@gmail.com>
2024-02-15 05:08:58 +0600

    old code sagor

--- README.md
+++
@@ -1,1 +0,0 @@
-"# Practice-alpha-pro" 
---
+++ js/utility.js
@@ -0,0 +1,26 @@
+function hideElement(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.add('hidden')
+}
+function showElement(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.remove('hidden')
+}
+
+function setBackgroundColorById(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.add('bg-orange-400')
+}
+
+
+function getRandomAlphabet(){
+    const alphaString = 'abcdefghijklmnopqrstuvwxyz';
+    const alpha = alphaString.split('');
+
+    const randomNumber = Math.random()*25;
+    const index = Math.round(randomNumber)
+
+
+    const alphabet = alpha[index];
+    return alphabet;
+}
